a864bcba8fee055b106e7f4975de354fa72f3b1f
NamuIsTree <54012447+NamuIsTree@users.noreply.github.com>
2020-03-23 17:26:15 +0900

    capstonProject1 1-Q3.9

--- 2week-Q3_9.py
+++ 2week-Q3_9.py
@@ -2,6 +2,7 @@ import random
 import time
 from requests import request
 for i in range(10):
-    value = random.randint(0, 60)
-    r = request('GET', "http://107.23.254.187:8000/" + str(value) + "%20HTTP/1.1")
-    print("sent")
+    x = random.randint(0, 60)
+    y = random.randint(0, 60)
+    z = random.randint(0, 60)
+    r = request('GET', 'http://107.23.254.187:8000/get?x=' + str(x) + '&y=' + str(y) + '&z=' + str(z) + "%20HTTP/1.1")
