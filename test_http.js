9dad29d304618474341e524e5abcd6a8e7be26e5
NamuIsTree <54012447+NamuIsTree@users.noreply.github.com>
2020-03-26 11:34:46 +0900

    Update test_http.js

--- test_http.js
+++ test_http.js
@@ -9,8 +9,6 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// code starts
-
 function getUserIP(req) {
 	const addr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	var str_addr = addr.toString();
